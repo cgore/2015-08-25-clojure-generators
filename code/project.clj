228a612e2ed96d6855b1364690b590519e7a17e7
Christopher Mark Gore <cgore@cgore.com>
2015-06-19 14:22:11 -0500

    Adding some example code

--- code/project.clj
+++ code/project.clj
@@ -1,4 +1,6 @@
 (defproject code "0.1.0-SNAPSHOT"
   :description "sample code"
   :url "http://www.cgore.com"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [prismatic/schema "0.4.3"]
+                 [org.clojure/test.check "0.7.0"]])
--- code/src/code/core.clj
+++ code/src/code/core.clj
@@ -1,6 +1,20 @@
-(ns code.core)
+(ns code.core
+  (:require [schema.core :as s]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Give us random integers (ten by default.)
+(gen/sample gen/int)
+
+;; Give us 20 random integers.
+(gen/sample gen/int 20)
+
+;; A schema for integers.
+(s/validate s/Int 42) ; passes, returns 42
+(try (s/validate s/Int "nope") ; fails, throws an error.
+     (catch Exception e)) ; we catch the error so this file will load cleanly
+
+;; If you don't want an exception use check instead of validate.
+(s/check s/Int 42) ; => nil
+(s/check s/Int "nope") ; => (not (integer? "nope"))
